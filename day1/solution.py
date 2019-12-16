fef0cc457bd0acef45a24343d4da7f787d076572
Arch-vile <mikko.ravimo@gmail.com>
2019-12-16 21:54:37 +0200

    Day 1 solution complete, one star

--- day1/solution.py
+++ day1/solution.py
@@ -1,2 +1,14 @@
-print("hello")
+from math import floor
 
+f = open("input.txt")
+
+
+def calculate_mass(x):
+    return floor(x / 3) - 2
+
+
+fuel = 0
+for moduleWeight in f:
+    fuel += calculate_mass(int(moduleWeight))
+
+print(fuel)
