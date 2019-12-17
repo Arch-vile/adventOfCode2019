ea3218c8bf08ef1293e0b46e208a5bc6e242a121
iravimo <ida.ravimo@gmail.com>
2019-12-17 20:54:28 +0200

    more extra fuel test pass

--- day1/solutionPart2.py
+++ day1/solutionPart2.py
@@ -1,8 +1,13 @@
-
 from math import floor
 
 
 def calculate_mass(x):
-    first_mass = floor(x / 3) - 2
-    second_mass = max(floor(first_mass / 3) - 2, 0)
-    return first_mass + second_mass
+    fuel = floor(x / 3) - 2
+    remaining_fuel = fuel
+
+    while remaining_fuel > 0:
+        new_remaining_fuel = max(floor(remaining_fuel / 3) - 2, 0)
+        fuel += new_remaining_fuel
+        remaining_fuel = new_remaining_fuel
+
+    return fuel
