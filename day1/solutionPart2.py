08ad355b0c83eda01123e244827fa58dc2a3b7af
Arch-vile <mikko.ravimo@gmail.com>
2019-12-17 21:32:59 +0200

    Add recursive solution

--- day1/solutionPart2.py
+++ day1/solutionPart2.py
@@ -2,16 +2,28 @@ import sys
 from math import floor
 
 
-def calculate_mass(x):
-    fuel = floor(x / 3) - 2
+def calculate_mass(mass):
+    fuel_need = needed_fuel(mass)
+    if fuel_need <= 0:
+        return 0
+    else:
+        return fuel_need + calculate_mass(fuel_need)
+
+
+def calculate_mass_old(x):
+    fuel = needed_fuel(x)
     remaining_fuel = fuel
 
     while remaining_fuel > 0:
-        new_remaining_fuel = max(floor(remaining_fuel / 3) - 2, 0)
+        new_remaining_fuel = max(needed_fuel(remaining_fuel), 0)
         fuel += new_remaining_fuel
         remaining_fuel = new_remaining_fuel
 
     return fuel
+
+
+def needed_fuel(remaining_fuel):
+    return floor(remaining_fuel / 3) - 2
 
 
 def process_input(input_file):
