d8711ae75b5be5c44d3528caa87e900d29704781
Arch-vile <mikko.ravimo@gmail.com>
2019-12-17 18:53:55 +0200

    Added test case stub

---
+++ day1/solutionTest.py
@@ -0,0 +1,17 @@
+import unittest
+
+from solution import calculate_mass
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_fuel(self):
+        self.assertEqual(calculate_mass(30), 8)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
