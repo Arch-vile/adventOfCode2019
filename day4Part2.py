7cf06a6afd7de50f6524fe74bf70085d1fce24b1
Arch-vile <mikko.ravimo@gmail.com>
2020-01-05 22:54:18 +0200

    Added some tests but maybe we read it wrong

--- day4Part2.py
+++ day4Part2.py
@@ -1,5 +1,7 @@
 import re
 
+from day4Part1 import check_adjacent_digits, check_increasing_digits
+
 
 def check_adjacent_digits_even(passcode):
     code_as_string = str(passcode)
@@ -9,3 +11,28 @@ def check_adjacent_digits_even(passcode):
         if len(match[0]) % 2 != 0:
             return False
     return True
+
+
+def test_password(passcode):
+    matchfirst = check_adjacent_digits(passcode) \
+                  & check_increasing_digits(passcode)
+    matchsecond = check_adjacent_digits(passcode) \
+           & check_increasing_digits(passcode) \
+           & check_adjacent_digits_even(passcode)
+
+    if (matchfirst is True) & (matchsecond is False):
+        # print(str(passcode))
+        return matchsecond
+
+    if matchfirst is not matchsecond:
+        print('fail')
+
+    return matchsecond
+
+
+def find_solution():
+    count = 0
+    for x in range(359282, 820401):
+        if test_password(x):
+            count += 1
+    print(count)
--- day4Part2Test.py
+++ day4Part2Test.py
@@ -1,7 +1,6 @@
-import re
 import unittest
 
-from day4Part2 import check_adjacent_digits_even
+from day4Part2 import check_adjacent_digits_even, find_solution, test_password
 
 
 class Day4Part1Tester(unittest.TestCase):
@@ -16,7 +15,32 @@ class Day4Part1Tester(unittest.TestCase):
         self.assertEqual(
             False, check_adjacent_digits_even(11234445)
         )
+        self.assertEqual(
+            True, check_adjacent_digits_even(22222222)
+        )
+        self.assertEqual(
+            False, check_adjacent_digits_even(222222222)
+        )
+        self.assertEqual(
+            True, check_adjacent_digits_even(1)
+        )
 
+    def test_matching(self):
+        self.assertEqual(
+            True, test_password(112233)
+        )
+        self.assertEqual(
+            False, test_password(123444)
+        )
+        self.assertEqual(
+            True, test_password(111122)
+        )
+        self.assertEqual(
+            True, test_password(8)
+        )
+
+    def test_solution(self):
+        print(find_solution())
 
     if __name__ == '__main__':
         unittest.main()
