4940c588390067a954b874571aa6a0e9a5aa3b4d
Arch-vile <mikko.ravimo@gmail.com>
2020-02-01 23:17:36 +0200

    simple chain calculation works

--- day5Part1.py
+++ day5Part1.py
@@ -1,6 +1,11 @@
 
-def calculate_orbits(body):
-    return 1
+def calculate_orbits(body, distance=0):
+    if not body.orbitors:
+        return distance
+    else:
+        orbits = map(lambda b: calculate_orbits(b, distance+1), body.orbitors)
+        return sum(orbits, distance)
+
 
 class Body:
     def __init__(self):
--- day6Tests.py
+++ day6Tests.py
@@ -6,12 +6,23 @@ from intcode_computer import run_program
 
 class MyTestCase(unittest.TestCase):
 
-    def test_foo(self):
+    def test_two_bodies(self):
         body1 = Body()
         body2 = Body()
         body1.add_orbitor(body2)
         self.assertEqual(
             1,
+            calculate_orbits(body1)
+        )
+
+    def test_simple_chain(self):
+        body1 = Body()
+        body2 = Body()
+        body3 = Body()
+        body1.add_orbitor(body2)
+        body2.add_orbitor(body3)
+        self.assertEqual(
+            3,
             calculate_orbits(body1)
         )
 
