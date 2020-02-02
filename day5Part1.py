b43d68508dc3d5d030b40388b508a886d21a919e
Arch-vile <mikko.ravimo@gmail.com>
2020-02-02 20:55:44 +0200

    Calculating orbits from input formatted data

--- day5Part1.py
+++ day5Part1.py
@@ -1,16 +1,34 @@
+# find body from dict, if not present then create and return
+def get_body(dictionary, body_name):
+    if body_name not in dictionary:
+        dictionary[body_name] = Body(body_name)
+
+    return dictionary[body_name]
+
+
+def calculate_orbits_from_map(map):
+    bodies_dict = {}
+    for orbit_relation in map:
+        body_names = orbit_relation.split(")")
+        body1 = get_body(bodies_dict, body_names[0])
+        body2 = get_body(bodies_dict, body_names[1])
+        body1.orbitors.append(body2)
+
+    return calculate_orbits(bodies_dict["COM"])
+
 
 def calculate_orbits(body, distance=0):
     if not body.orbitors:
         return distance
     else:
-        orbits = map(lambda b: calculate_orbits(b, distance+1), body.orbitors)
+        orbits = map(lambda b: calculate_orbits(b, distance + 1), body.orbitors)
         return sum(orbits, distance)
 
 
 class Body:
-    def __init__(self):
+    def __init__(self, name):
         self.orbitors = []
+        self.name = name
 
     def add_orbitor(self, body):
         self.orbitors.append(body)
-
--- day6Tests.py
+++ day6Tests.py
@@ -1,14 +1,13 @@
 import unittest
 
-from day5Part1 import Body, calculate_orbits
-from intcode_computer import run_program
+from day5Part1 import Body, calculate_orbits, calculate_orbits_from_map
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_two_bodies(self):
-        body1 = Body()
-        body2 = Body()
+        body1 = Body("1")
+        body2 = Body("2")
         body1.add_orbitor(body2)
         self.assertEqual(
             1,
@@ -16,9 +15,9 @@ class MyTestCase(unittest.TestCase):
         )
 
     def test_simple_chain(self):
-        body1 = Body()
-        body2 = Body()
-        body3 = Body()
+        body1 = Body("1")
+        body2 = Body("2")
+        body3 = Body("3")
         body1.add_orbitor(body2)
         body2.add_orbitor(body3)
         self.assertEqual(
@@ -27,16 +26,22 @@ class MyTestCase(unittest.TestCase):
         )
 
     def test_simple_branch(self):
-        body1 = Body()
-        body2 = Body()
-        body3 = Body()
-        body4 = Body()
+        body1 = Body("1")
+        body2 = Body("2")
+        body3 = Body("3")
+        body4 = Body("4")
         body1.add_orbitor(body2)
         body2.add_orbitor(body3)
         body2.add_orbitor(body4)
         self.assertEqual(
             5,
             calculate_orbits(body1)
+        )
+
+    def test_read_input(self):
+        self.assertEqual(
+            5,
+            calculate_orbits_from_map(["COM)2", "2)3", "2)4"])
         )
 
 
